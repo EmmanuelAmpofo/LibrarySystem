70a13fd32e27fb3f00c4dfb0c50952e7f5e135b4
EmmanuelAmpofo <emmanuel.ampofo@amalitech.org>
2022-09-30 15:10:09 +0000

    Sign up password input validation

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 function SignUp() {
   const [input, setInput] = useState("");
+  const [error, setError] = useState("")
 
   const [capsLock, setCapsLock] = useState(false);
   const [visiblePassword, setVisiblePassword] = useState(false);
@@ -31,6 +32,7 @@ function SignUp() {
       ...prev,
       [id]: value,
     }));
+    comparePassword(event)
   };
   const handleSignUp = (event) => {
     event.preventDefault();
@@ -54,6 +56,40 @@ function SignUp() {
       alert("Please all form fields are required");
     }
   };
+
+  const comparePassword = (event) =>{
+    const {id, value} = event.target;
+    setError(prev => {
+      const errorStateObj = { 
+        ...prev, [id]: ""
+      }
+      switch (id) {
+        case "password":
+          if(!value){
+            errorStateObj[id] = "Please enter your password.";
+          } else if (input.confirmPassword && value !== input.confirmPassword){
+            errorStateObj["confirmPassword"] = "Password and Confirm Password does not match."
+          } else{
+            errorStateObj["confirmPassword"] = input.confirmPassword ? "" : error.confirmPassword;
+          }
+          break;
+
+        case "confirmPassword":
+          if(!value){
+            errorStateObj[id] =  "Please confirm password.";
+          }else if (input.password && value !== input.password){
+            errorStateObj[id] = "Password and confirm password doies not match.";
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      return errorStateObj;
+    })
+  }
+
   return (
     <Box className="loginWrapper">
       <form action="" className="signUpFormWrapper">
@@ -122,8 +158,10 @@ function SignUp() {
             autoComplete="off"
             required={true}
             onChange={handleInputChange}
+            onBlur={comparePassword}
             value={input.password}
           />
+          {error.password && <span className="caps">{error.password}</span>}
           <label htmlFor="confirmPassword">Confirm Password:</label>
           <Input
             onKeyUp={checkCapsLockOn}
@@ -137,15 +175,15 @@ function SignUp() {
             onChange={handleInputChange}
             value={input.confirmPassword}
           />
-          <Box className="pwdWrapper">
+          <Box className="pwdWrapper" onClick={showPassword}>
             <Box className="togglePwd">
               <input
                 type="checkbox"
                 name="show password"
-                id=""
+                id="vvv"
                 onClick={showPassword}
               />
-              <span className="shwPwd">show password</span>
+              <label htmlFor="vvv" className="shwPwd">show password</label>
             </Box>
           </Box>
           <button type="submit" className="loginBtn" onClick={handleSignUp}>
@@ -154,9 +192,9 @@ function SignUp() {
         </Box>
         <span className="dontHave">
           Already have an account?{" "}
-          <span onClick={routeToPage} className="haveAccount">
+          <label onClick={routeToPage} className="haveAccount">
             Log In
-          </span>
+          </label>
         </span>
       </form>
     </Box>
